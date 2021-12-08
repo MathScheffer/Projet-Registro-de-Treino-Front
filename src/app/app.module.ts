4d06b1092860388e9b2ed271f0ec9fd62f7a14f2
MathScheffer <matheusmartinsscheffer@hotmail.com>
2021-12-08 12:41:38 -0300

    iniciar separação dos components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RotinaComponent } from './rotina/rotina.component';
-import { CardRotinaComponent } from './card-rotina/card-rotina.component';
+import { CardRotinaComponent } from './components/card-rotina/card-rotina.component';
 import { CardListComponent } from './card-list/card-list.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { LeftComponent } from './left/left.component';
--- src/app/components/card-rotina/card-rotina.component.ts
+++ src/app/components/card-rotina/card-rotina.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RotinaService } from '../service/rotina.service';
+import { RotinaService } from '../../service/rotina.service';
 
 @Component({
   selector: 'card-rotina',
